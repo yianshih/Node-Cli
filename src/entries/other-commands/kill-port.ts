15e38e3de08b6eaf57f898a4d1d22754bcaab6f3
Yi-an Shih <yian.shih@kindredgroup.com>
2022-04-08 12:08:18 +1000

    add kill pid function

--- src/entries/other-commands/kill-port.ts
+++ src/entries/other-commands/kill-port.ts
@@ -1,12 +1,25 @@
 import { exec } from "@cli/utils/exec";
 import { logger } from "@cli/utils/logger";
+import { parseTableOutputToObject } from "@cli/utils/parsers";
 import { prompt, QuestionCollection } from "inquirer";
+
+interface Lsof {
+  COMMAND: string;
+  PID: string;
+  USER: string;
+  FD: string;
+  TYPE: string;
+  DEVICE: string;
+  "SIZE/OFF": string;
+  NODE: string;
+  NAME: string;
+}
 
 interface KillPortAnswer {
   port: string;
 }
 interface KillPIDAnswer {
-  pid: string;
+  pids: string[];
 }
 
 const GetPortMenu: QuestionCollection<KillPortAnswer>[] = [
@@ -17,13 +30,54 @@ const GetPortMenu: QuestionCollection<KillPortAnswer>[] = [
   }
 ];
 
-const GetPIDMenu: QuestionCollection<KillPIDAnswer>[] = [
+const GetPIDMenu = (choices: string[]): QuestionCollection<KillPIDAnswer>[] => [
   {
-    name: "pid",
+    name: "pids",
     message: "Which PID you would like to kill ?",
-    type: "input"
+    type: "checkbox",
+    choices: [...new Set(choices)]
   }
 ];
+
+const defaultLsof: Lsof = {
+  COMMAND: "",
+  PID: "",
+  USER: "",
+  FD: "",
+  TYPE: "",
+  DEVICE: "",
+  "SIZE/OFF": "",
+  NODE: "",
+  NAME: ""
+};
+
+const parsedLosfLine = (data: string): string[] => {
+  const values = data.split(" ");
+  const content = values.slice(0, 8);
+  // Name may contain space
+  const name = values.slice(8);
+  return [...content, name.join(" ")];
+};
+
+const getLsofCommand = (port: string) => `lsof -i tcp:${port}`;
+const getKillCommand = (pids: string[]) => `kill ${pids.join(" ")}`;
+
+const killPid = async (data: Lsof[]) => {
+  const { pids = [] } = await prompt(GetPIDMenu(data.map((d) => d.PID)));
+  if (!pids.length) {
+    logger.warning("Please select PID to kill");
+    killPid(data);
+    return;
+  }
+  exec(getKillCommand(pids))(
+    (o) => {
+      logger.info(o);
+    },
+    (e) => {
+      logger.error(e);
+    }
+  );
+};
 
 export const main = async () => {
   const answer = await prompt(GetPortMenu);
@@ -33,10 +87,11 @@ export const main = async () => {
     main();
     return;
   }
-  exec(`lsof -i tcp:${port}`)(
+  exec(getLsofCommand(port))(
     async (o) => {
+      const data = parseTableOutputToObject(o, defaultLsof, parsedLosfLine);
       logger.info(o);
-      const { pid } = await prompt(GetPIDMenu);
+      await killPid(data);
     },
     (e, o) => {
       if (!o) logger.info(`Port ${port} is not in use`);
---
+++ src/utils/parsers.ts
@@ -0,0 +1,33 @@
+const trimMultiSpaces = (value: string) => value.replace(/\s+/g, " ").trim();
+
+const isHeaderValid = (headers: string[], obj: object): boolean => {
+  const headersSet = new Set(headers);
+  const objSet = new Set(Object.keys(obj));
+  // if headers has same structure of obj, the set of headers will have the same size of header Set and obj Set combined
+  return headersSet.size === new Set([...headersSet, ...objSet]).size;
+};
+
+const arrayToObject = <T extends object>(arr: string[], defaultObj: T): T => {
+  const keys = Object.keys(defaultObj);
+  if (keys.length !== arr.length) return defaultObj;
+  return Object.fromEntries(keys.map((k, i) => [k, arr[i]])) as T;
+};
+
+export const parseTableOutputToObject = <T extends object>(
+  output: string,
+  defaultObj: T,
+  parseContent?: (data: string) => string[]
+): T[] => {
+  const lines = output.split("\n");
+  if (!lines.length) return [defaultObj];
+  const [header, ...contentLines] = lines.filter((l) => l !== "");
+  const headers = trimMultiSpaces(header).split(" ");
+  if (!isHeaderValid(headers, defaultObj)) return [defaultObj];
+  return contentLines.map((content) => {
+    content = trimMultiSpaces(content);
+    const parsedContent = parseContent
+      ? parseContent(content)
+      : content.split(" ");
+    return arrayToObject(parsedContent, defaultObj);
+  });
+};
